191eff48c963b69a98a36cfea6f566e0688b9a7b
EunaRede <rede@eunarede.com>
2016-09-17 17:53:26 -0300

    Tradução para Português
    Tradução das variaveis do modulo para portugues do brasil

---
+++ modules/servers/namecheapssl/lang/portuguese-br.php
@@ -0,0 +1,205 @@
+<?php
+
+$_MOD_LANG['ncssl_configure_certificate'] = "Configurar Certificadi";
+$_MOD_LANG['ncssl_resend_certificate'] = "Reenviar Certificado";
+$_MOD_LANG['ncssl_resend_approver_email'] = "Reenviar Email de Aprovação";
+$_MOD_LANG['ncssl_view_certificate_details'] = "Ver Detalhes do Certificado";
+$_MOD_LANG['ncssl_certificate_details'] = "Detalhes do Certificado";
+//$_MOD_LANG['ncssl_resend_config_email'] = "Resend Configuration Email";
+$_MOD_LANG['ncssl_certificate_details_for'] = "Detalhes de certificados para";
+$_MOD_LANG['ncssl_domain'] = "Domínio";
+$_MOD_LANG['ncssl_status'] = "Status";
+$_MOD_LANG['ncssl_type'] = "Tipo";
+$_MOD_LANG['ncssl_approver_email'] = "Email de Aprovação";
+$_MOD_LANG['ncssl_admin_email'] = "Email do Admnistrador";
+$_MOD_LANG['ncssl_no_auto_approve'] = "Infelizmente, neste momento o envio de e-mail de aprovação para este tipo de certificado SSL não é possível a partir desta interface. Entre em contato com o suporte para obter assistência.";
+$_MOD_LANG['ncssl_no_certificate_exists'] = "Nenhum pedido de SSL existente";
+$_MOD_LANG['ncssl_no_incomplete_certificate'] = "No Incomplete SSL Order exists for this order";
+$_MOD_LANG['ncssl_error_occured'] = "Ocorreu um erro: ";
+$_MOD_LANG['ncssl_months'] = "Meses";
+$_MOD_LANG['ncssl_unable_retrieve_certtypes'] = "Não foi possível recuperar a lista de tipos de certificados suportados.";
+$_MOD_LANG['ncssl_try_again_in_several_minutes'] = "Por favor tente novamente em alguns minutos";
+$_MOD_LANG['ncssl_sslorder_already_exists'] = "Já existe um SSL para este pedido";
+$_MOD_LANG['ncssl_please_contact_support'] = "Por favor contate o suporte";
+$_MOD_LANG['ncssl_reissue_certificate'] = 'Reemitir Certificado';
+$_MOD_LANG['ncssl_client_reissue_notice_1'] = 'Para reemitir o certificado, por favor acesse o link:';
+$_MOD_LANG['ncssl_client_reissue_notice_2'] = 'Uma vez logado, clique no botão "Solicitação de Acesso". Um e-mail de confirmação com um link de acesso será enviado para o e-mail indicado durante o login. Para reemitir o certificado, siga o link no e-mail, clique no botão "Reemitir certificado", e configure o seu certificado SSL.';
+
+
+$_MOD_LANG['ncssl_comodo_add_form_title'] = "Verificação dos detalhes do contato. Informações da Organização/Empresa";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepFirstName'] = "Primeiro Nome do representante*";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepLastName'] = "Sobrenome do representante*";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepTitle'] = "Cargo do representante";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepEmailAddress'] = 'E-mail*';
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepEmailAddress_Description'] = "Endereço de e-mail a ser usado para verificação pela COMODO";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepPhone'] = "Telefone*";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepFax'] = "Fax";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepCallbackMethod'] = "Selecione o método de callback*";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepCallbackDestinationSame_Description'] = "O nome da Organização/Endereço são diferentes da Organização/Endereço do contato adminsitrativo?";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepLegalName'] = "Nome jurídico do Representante*";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepAddress1'] = "Endereço do Representante*";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepAddress2'] = "Número/Bairro do endereço do Representante*";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepCity'] = "Cidade*";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepStateProvince'] = "Estado*";
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepPostalCode'] = 'CEP*';
+$_MOD_LANG['ncssl_comodo_add_OrganizationRepCountry'] = 'País*';
+
+
+$_MOD_LANG['ncssl_comodo_ev_add_form_title'] = 'Detalhes da Empresa para Validação Extendida';
+$_MOD_LANG['ncssl_comodo_ev_add_CompanyIncorporationCountry'] = 'País de constituição da Empresa*';
+$_MOD_LANG['ncssl_comodo_ev_add_CompanyIncorporationLocality'] = 'Cidade de constituição da Empresa';
+$_MOD_LANG['ncssl_comodo_ev_add_CompanyIncorporationStateProvince'] = 'Estado de constituição da Empresa';
+$_MOD_LANG['ncssl_comodo_ev_add_CompanyIncorporationDate'] = 'Data de fundação da Empresa';
+$_MOD_LANG['ncssl_comodo_ev_add_CompanyIncorporationDateDescription'] = 'Insira a data no formato norte-americano, mm/dd/YYYY (mês/dia/ano)';
+$_MOD_LANG['ncssl_comodo_ev_add_CompanyDBA'] = 'Nome Fantasia da Empresa';
+$_MOD_LANG['ncssl_comodo_ev_add_CompanyRegistrationNumber'] = 'CNPJ da Empresa';
+
+
+$_MOD_LANG['ncssl_oth_ev_ov_add_form_title'] = 'Detalhes da Organização';
+$_MOD_LANG['ncssl_oth_ev_ov_add_OrganizationLegalName'] = 'Razão Social da Organização*';
+$_MOD_LANG['ncssl_oth_ev_ov_add_OrganizationDUNS'] = 'Número DUNS da Organização';
+$_MOD_LANG['ncssl_oth_ev_ov_add_OrganizationAddress1'] = 'Endereço da Organização*';
+$_MOD_LANG['ncssl_oth_ev_ov_add_OrganizationAddress2'] = 'Número/Bairro do Enredeço da Organização';
+$_MOD_LANG['ncssl_oth_ev_ov_add_OrganizationCity'] = 'Cidade da Organização*';
+$_MOD_LANG['ncssl_oth_ev_ov_add_OrganizationStateProvince'] = 'Estado da Organização*';
+$_MOD_LANG['ncssl_oth_ev_ov_add_OrganizationPostalCode'] = 'CEP da Organização*';
+$_MOD_LANG['ncssl_oth_ev_ov_add_OrganizationCountry'] = 'País da Organização*';
+$_MOD_LANG['ncssl_oth_ev_ov_add_OrganizationPhone'] = 'Número de telefone no formato +NNN.NNNNNNNNNN*';
+$_MOD_LANG['ncssl_oth_ev_ov_add_OrganizationFax'] = 'Número de fax no formato +NNN.NNNNNNNNNN*';
+
+
+$_MOD_LANG['ncssl_admin_viewdetails_certificate_details'] = 'Detalhes do certificado';
+$_MOD_LANG['ncssl_admin_viewdetails_status'] = 'Status';
+$_MOD_LANG['ncssl_admin_viewdetails_status_description'] = 'Descrição do Status';
+$_MOD_LANG['ncssl_admin_viewdetails_type'] = 'Tipo';
+$_MOD_LANG['ncssl_admin_viewdetails_issued_on'] = 'Emitido em';
+$_MOD_LANG['ncssl_admin_viewdetails_expires'] = 'Expira em';
+$_MOD_LANG['ncssl_admin_viewdetails_activation_expire_date'] = 'Activation Expire Date';
+$_MOD_LANG['ncssl_admin_viewdetails_nc_order_id'] = 'NC Order id';
+$_MOD_LANG['ncssl_admin_viewdetails_nc_cert_id'] = 'NC Cert id';
+$_MOD_LANG['ncssl_admin_viewdetails_csr'] = 'CSR';
+$_MOD_LANG['ncssl_admin_viewdetails_decoded_csr'] = 'Decodificar CSR';
+$_MOD_LANG['ncssl_admin_viewdetails_common_name'] = 'Common Name';
+$_MOD_LANG['ncssl_admin_viewdetails_domain_name'] = 'Domain Name';
+$_MOD_LANG['ncssl_admin_viewdetails_country'] = 'Country';
+$_MOD_LANG['ncssl_admin_viewdetails_organization_unit'] = 'Organization Unit';
+$_MOD_LANG['ncssl_admin_viewdetails_organization'] = 'Organization';
+$_MOD_LANG['ncssl_admin_viewdetails_valid_true_domain'] = 'Valid True Domain';
+$_MOD_LANG['ncssl_admin_viewdetails_state'] = 'State';
+$_MOD_LANG['ncssl_admin_viewdetails_locality'] = 'Locality';
+$_MOD_LANG['ncssl_admin_viewdetails_email'] = 'Email';
+$_MOD_LANG['ncssl_admin_viewdetails_approver_email'] = 'Approver Email';
+$_MOD_LANG['ncssl_admin_viewdetails_common_name'] = 'Common Name';
+$_MOD_LANG['ncssl_admin_viewdetails_provider_order_id'] = 'Provider Order ID';
+$_MOD_LANG['ncssl_admin_viewdetails_provider_name'] = 'Provider Name';
+$_MOD_LANG['ncssl_admin_viewdetails_button'] = 'View Info';
+
+$_MOD_LANG['ncssl_admin_viewdetails_count_of_sans'] = 'Count of SANs';
+$_MOD_LANG['ncssl_admin_viewdetails_sans'] = 'Domínios Adicionais';
+
+
+$_MOD_LANG['ncssl_addon_menu_log'] = 'Log';
+$_MOD_LANG['ncssl_addon_menu_sync'] = 'Link/Re-Link WHMCS Cert ID to NC Cert ID';
+$_MOD_LANG['ncssl_addon_menu_list'] = 'Lista de Certificado';
+$_MOD_LANG['ncssl_addon_menu_settings'] = 'Configurações';
+
+
+$_MOD_LANG['ncssl_addon_list_user_list'] = 'User list';
+$_MOD_LANG['ncssl_addon_list_certificate_id'] = 'Certificate ID';
+$_MOD_LANG['ncssl_addon_list_host_name'] = 'Host Name';
+$_MOD_LANG['ncssl_addon_list_type'] = 'Type';
+$_MOD_LANG['ncssl_addon_list_purchase_date'] = 'Purchase Date';
+$_MOD_LANG['ncssl_addon_list_expire_date'] = 'Expire Date';
+$_MOD_LANG['ncssl_addon_list_activation_expire_date'] = 'Activaton Expire Date';
+$_MOD_LANG['ncssl_addon_list_expired'] = 'Expired';
+$_MOD_LANG['ncssl_addon_list_status'] = 'Status';
+$_MOD_LANG['ncssl_addon_list_whmcs_product_id'] = 'WHMCS Product ID';
+$_MOD_LANG['ncssl_addon_list_whmcs_cert_status'] = 'WHMCS Cert Status';
+$_MOD_LANG['ncssl_addon_list_pages'] = 'Pages';
+
+$_MOD_LANG['ncssl_addon_sync_specify_product_id'] = 'Specify WHMCS Product ID';
+$_MOD_LANG['ncssl_addon_sync_search_button'] = 'Search';
+$_MOD_LANG['ncssl_addon_sync_not_found'] = 'Not found';
+$_MOD_LANG['ncssl_addon_sync_success'] = 'Remote ID assigned successfully';
+$_MOD_LANG['ncssl_addon_sync_order_no'] = 'Order #';
+$_MOD_LANG['ncssl_addon_sync_product_service'] = 'Product/Service';
+$_MOD_LANG['ncssl_addon_sync_domain'] = 'Domain';
+$_MOD_LANG['ncssl_addon_sync_cert_type'] = 'Cert type';
+$_MOD_LANG['ncssl_addon_sync_remote_id'] = 'REMOTE ID';
+$_MOD_LANG['ncssl_addon_sync_specify_remote_id'] = 'Specify NC Remote ID for Sync';
+$_MOD_LANG['ncssl_addon_sync_button'] = 'Sync';
+
+$_MOD_LANG['ncssl_addon_sync_notice'] = 'Please use this function only for failed orders!';
+$_MOD_LANG['ncssl_addon_sync_notice_san'] = 'Linking Multi-domain certificates is not possible in current version.';
+
+
+$_MOD_LANG['ncssl_addon_log_records_found'] = 'Records Found';
+$_MOD_LANG['ncssl_addon_log_page'] = 'Page';
+$_MOD_LANG['ncssl_addon_log_of'] = 'of';
+$_MOD_LANG['ncssl_addon_log_jump_to_page'] = 'Jump to Page';
+$_MOD_LANG['ncssl_addon_log_choose_action'] = 'Choose action';
+$_MOD_LANG['ncssl_addon_log_start_date'] = 'Start date';
+$_MOD_LANG['ncssl_addon_log_end_date'] = 'End date';
+$_MOD_LANG['ncssl_addon_log_user_input_placeholder'] = 'Admin login/client email';
+$_MOD_LANG['ncssl_addon_log_filter_button'] = 'Filter';
+$_MOD_LANG['ncssl_addon_log_date'] = 'Date';
+$_MOD_LANG['ncssl_addon_log_action'] = 'Action';
+$_MOD_LANG['ncssl_addon_log_description'] = 'Description';
+$_MOD_LANG['ncssl_addon_log_serviceid'] = 'Service ID';
+$_MOD_LANG['ncssl_addon_log_username'] = 'Username';
+$_MOD_LANG['ncssl_addon_log_ipaddress'] = 'IP Address';
+$_MOD_LANG['ncssl_addon_sun_reduction_notice'] = 'Decreasing number of addon domains is not allowed. Please go back.';
+
+
+$_MOD_LANG['ncssl_addon_setting_ssync_date_offset_1'] = 'Product due date sync offset';
+$_MOD_LANG['ncssl_addon_setting_ssync_date_offset_2'] = 'days before the actual expiration date';
+
+$_MOD_LANG['ncssl_addon_changes_saved_success'] = 'Changes Saved Successfully!';
+
+
+$_MOD_LANG['ncssl_http_based_validation'] = "HTTP based validation";
+$_MOD_LANG['ncssl_show_validation_file_contents'] = 'Show validation file contents';
+$_MOD_LANG['ncssl_custom_phrase_sslconfigcompletedetails'] = 'Please create<br /><br />%filename%<br /><br />with contents<br /><br />%contents%<br /><br />and put it into the root directory of your domain. It may take some time for file to be automatically validated and cert to be issued by Certificate Authority';
+$_MOD_LANG['ncssl_download_certificate'] = "Download certificate";
+
+
+$_MOD_LANG['ncssl_custom_phrase_sslcertapproveremaildetails'] = 'Please select any of emails if you wish to proceed with email-based validation. An approval email will be sent to selected address by Certificate Authority after activation.';
+$_MOD_LANG['ncssl_custom_phrase_sslcertapproveremaildetails_http_based'] = '<br /><br />If you select \'HTTP based validation\' option,  you will need to place a certain file into a root directory of your domain. Name and contents of the file will be provided at the last step of the activation process. No emails will be sent for Domain Control Validation.';
+
+$_MOD_LANG['ncssl_custom_phrase_sslcertapproveremaildetails_san'] = 'Please select local part of emails you wish to receive your approval emails to. This local part will be used to send approval emails for all domains and subdomains. <br /><br />For example, if \'admin@\' is selected as approver having main domain specified as \'domain.com\' and addon domain is \'sub.domain1.com\' , the approver emails will be sent to admin@domain.com and admin@sub.domain1.com.';
+
+$_MOD_LANG['ncssl_custom_phrase_sslcertapproveremaildetails_san_http_based'] = '<br /><br />If you select \'HTTP based validation\' option, you will need to place a certain file into a root directory of servers for all of the domains specified while activation. Name and contents of the file will be provided at the last step of the activation process. No emails will be sent for Domain Control Validation.';
+
+
+$_MOD_LANG['ncssl_san_items_title'] = 'Enter additional domains';
+
+$_MOD_LANG['ncssl_san_items_item'] = 'Addon Domain ';
+$_MOD_LANG['ncssl_san_items_item_quick_ssl_premium'] = 'Addon Subdomain ';
+
+$_MOD_LANG['ncssl_san_csr_note'] = ' Domains included in SAN sections in CSR will be ignored! Please input addon domains in fields below the box for CSR. Please do not input main domain as addon domain. It is taken from CSR. Please make sure you do not use duplicates.';
+
+$_MOD_LANG['ncssl_reissue_notice_step1'] = 'The following contact information will be used for reissue of your certificate:';
+$_MOD_LANG['ncssl_reissue_notice_step2'] = 'The following information will be used for reissue of your certificate:';
+
+$_MOD_LANG['ncssl_revoke_confirmation_text'] = 'Are you sure that you want to revoke the old certificates associated with this order? This will not affect latest active certificate, only the old reissued certificates will be revoked.';
+$_MOD_LANG['ncssl_revoke_button'] = 'Revoke old certificates';
+$_MOD_LANG['ncssl_revoke_button_description'] = 'Recommended before new reissue';
+
+$_MOD_LANG['ncssl_error_2011166'] = 'Invalid domain name. Please use the domain name used during initial activation: ';
+
+
+$_MOD_LANG['ncssl_error_revoke_1'] = 'Revocation of reissued certificates associated with this order is unavailable right away due to limitations from Symantec. Please contact support for revocation of your reissued certificates';
+$_MOD_LANG['ncssl_error_revoke_2'] = 'Your revocation request is being processed manually by admin. Revocation will be completed as soon as possible';
+$_MOD_LANG['ncssl_error_revoke_3'] = 'Your revocation request is being processed manually. Revocation will be completed as soon as possible';
+$_MOD_LANG['ncssl_error_revoke_4'] = 'Revocation of multiple instances of reissued certificates is temporary unavailable due to technical restrictions at Certificate Authority’s side. Please contact support for further assistance.';
+
+
+$_MOD_LANG['ncssl_back_to_service'] = '&laquo; Back to the service';
+
+$_MOD_LANG['ncsssl_jobtitle_required_for_symantec'] = 'Job Title is required';
+
+
+$_MOD_LANG['ncssl_symantec_approver_email_notice'] = 'Approval email is not required for this type of certificate';
+
+
+$_MOD_LANG['ncssl_latin_characters_notice'] = 'Only Latin characters can be used in the Admin information';
